475b6a36dfeb13b371d3ed50433d3d51b846bf23
Sergio Rodrigues <sergioaugrod@gmail.com>
2023-07-22 16:43:40 -0300

    change the colorscheme

---
+++ nvim/after/plugin/catppuccin.lua
@@ -0,0 +1,8 @@
+require('catppuccin').setup({
+  flavour = 'frappe',
+  transparent_background = true,
+  term_colors = true,
+  no_italic = true,
+})
+
+vim.cmd.colorscheme('catppuccin')
--- nvim/after/plugin/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-require('vscode').setup({
-  style = 'light',
-  transparent = false,
-  disable_nvimtree_bg = true,
-})
-
-require('vscode').load()
