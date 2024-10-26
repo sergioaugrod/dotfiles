c8e1f5515df58efb158b56de319b711bb2197ab3
Sérgio Rodrigues <sergioaugrod@gmail.com>
2024-10-26 15:12:15 -0300

    update nvim deps

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -22,20 +22,19 @@ return {
   {
     {
       'CopilotC-Nvim/CopilotChat.nvim',
-      opts = {
-        show_help = 'yes',         -- Show help text for CopilotChatInPlace, default: yes
-        debug = false,             -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-        disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
-      },
-      build = function()
-        vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-      end,
+      branch = 'canary',
       event = 'VeryLazy',
+      dependencies = {
+        { 'zbirenbaum/copilot.lua' },
+        { 'nvim-lua/plenary.nvim' },
+      },
+      build = 'make tiktoken',
       keys = {
         { '<leader>ccc', ':CopilotChat ',               desc = 'CopilotChat - Prompt' },
+        { '<leader>ccr', '<cmd>CopilotChatReview<cr>',  desc = 'CopilotChat - Review code' },
         { '<leader>cce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
+        { '<leader>ccd', '<cmd>CopilotChatDocs<cr>',    desc = 'CopilotChat - Add docs' },
         { '<leader>cct', '<cmd>CopilotChatTests<cr>',   desc = 'CopilotChat - Generate tests' },
-        { '<leader>ccr', '<cmd>CopilotChatReset<cr>',   desc = 'CopilotChat - Reset chat history and clear buffer' },
       },
     },
   }
