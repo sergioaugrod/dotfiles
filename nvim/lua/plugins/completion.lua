48507849c162d1b8d93ca46173984f38aef2e20a
Sérgio Rodrigues <sergioaugrod@gmail.com>
2025-10-25 11:01:28 -0300

    cleanup some cmps

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,9 +2,7 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp'
   },
   config = function()
     local cmp = require('cmp')
@@ -19,15 +17,9 @@ return {
         ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       }),
-      sources = cmp.config.sources(
-        {
-          { name = 'nvim_lsp' },
-          { name = 'path' },
-        },
-        {
-          { name = 'buffer' },
-        }
-      ),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' }
+      })
     })
   end,
 }
